13f22c27e608f2c00af6bdfde78d89567d08b2bc
Dotnet9(dotnet9.com) <1012434131@qq.com>
2024-02-27 13:55:07 +0800

    Update PostEntity.cs

--- src/CodeWF.Data/Entities/PostEntity.cs
+++ src/CodeWF.Data/Entities/PostEntity.cs
@@ -10,14 +10,14 @@ public class PostEntity
     }
 
     public Guid Id { get; set; }
-    public string Title { get; set; }
-    public string Slug { get; set; }
-    public string Author { get; set; }
-    public string PostContent { get; set; }
+    public string? Title { get; set; }
+    public string? Slug { get; set; }
+    public string? Author { get; set; }
+    public string? PostContent { get; set; }
     public bool CommentEnabled { get; set; }
     public DateTime CreateTimeUtc { get; set; }
-    public string ContentAbstract { get; set; }
-    public string ContentLanguageCode { get; set; }
+    public string? ContentAbstract { get; set; }
+    public string? ContentLanguageCode { get; set; }
     public bool IsFeedIncluded { get; set; }
     public DateTime? PubDateUtc { get; set; }
     public DateTime? LastModifiedUtc { get; set; }
@@ -25,8 +25,8 @@ public class PostEntity
     public bool IsDeleted { get; set; }
     public bool IsOriginal { get; set; }
     public bool IsOutdated { get; set; }
-    public string OriginLink { get; set; }
-    public string HeroImageUrl { get; set; }
+    public string? OriginLink { get; set; }
+    public string? HeroImageUrl { get; set; }
     public bool IsFeatured { get; set; }
     public int HashCheckSum { get; set; }
 
