7cf26a3ee6a195af7bf6dd5ca04542008a11e05b
Dotnet9(dotnet9.com) <1012434131@qq.com>
2024-06-19 22:26:46 +0800

    add user entity;

---
+++ src/CodeWF.Data/Entities/User.cs
@@ -0,0 +1,57 @@
+﻿namespace CodeWF.Data.Entities;
+
+public class User
+{
+    public Guid Id { get; set; }
+
+    public string UserName { get; set; }
+    public string Password { get; set; }
+
+    public string? PhoneNumber { get; set; }
+
+    public string? Email { get; set; }
+
+    public EnabledKind UserStatus { get; set; }
+
+    public GenderKind Gender { get; set; }
+
+    public string? OpenId { get; set; }
+
+    public string? Avatar { get; set; }
+
+    public string? Admire { get; set; }
+
+    public string? Subscribe { get; set; }
+
+    public string? Introduction { get; set; }
+
+    public UserTypeKind UserType { get; set; }
+
+    public DateTime CreateTime { get; set; }
+
+    public DateTime UpdateTime { get; set; }
+
+    public string? UpdateBy { get; set; }
+
+    public EnabledKind Deleted { get; set; }
+}
+
+public enum EnabledKind
+{
+    No = 0,
+    Yes = 1
+}
+
+public enum GenderKind
+{
+    Male = 1,
+    Female = 2,
+    Unspecified = 0
+}
+
+public enum UserTypeKind
+{
+    Admin = 0,
+    Manager = 1,
+    Regular = 2
+}
