5a82e41e19cf9d4e1dbfdfcf8f974a1028e1eb79
Dotnet9(dotnet9.com) <1012434131@qq.com>
2024-07-11 23:06:12 +0800

    add empty blazor web app;

---
+++ src/CodeWF/CodeWF/Program.cs
@@ -0,0 +1,31 @@
+using CodeWF.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(CodeWF.Client._Imports).Assembly);
+
+app.Run();
