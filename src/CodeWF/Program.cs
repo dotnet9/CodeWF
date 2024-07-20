4bcaf8add1081407ce77396c824e6ed44da58e74
Dotnet9(dotnet9.com) <1012434131@qq.com>
2024-07-20 16:16:26 +0800

    fix style;

--- src/CodeWF/Program.cs
+++ src/CodeWF/Program.cs
@@ -21,7 +21,7 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    //app.UseHsts();
 }
 
 app.UseHttpsRedirection();
