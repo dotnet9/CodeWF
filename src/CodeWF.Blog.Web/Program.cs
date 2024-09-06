4721b5a0e8a2694d5077efb0fdfd2dcc15598d36
Dotnet9(dotnet9.com) <1012434131@qq.com>
2024-09-06 23:34:25 +0800

    add blog;

---
+++ src/CodeWF.Blog.Web/Program.cs
@@ -0,0 +1,33 @@
+using CodeWF.Blog.Web.Client.Pages;
+using CodeWF.Blog.Web.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+builder.Services.AddFluentUIComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(CodeWF.Blog.Web.Client._Imports).Assembly);
+
+app.Run();
