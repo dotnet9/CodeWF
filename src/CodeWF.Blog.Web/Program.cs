0dcc91642e29a28657221b5f3bf22e1fd30b83af
Dotnet9(dotnet9.com) <1012434131@qq.com>
2024-09-25 14:25:53 +0800

    fix cdn;

--- src/CodeWF.Blog.Web/Program.cs
+++ src/CodeWF.Blog.Web/Program.cs
@@ -3,18 +3,22 @@ using CodeWF.Blog.Web.Client.Options;
 using CodeWF.Blog.Web.Client.Services;
 using CodeWF.Blog.Web.Components;
 using Microsoft.FluentUI.AspNetCore.Components;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.Configure<SiteOption>(builder.Configuration.GetSection("Site"));
 builder.Services.AddSingleton<IBlogPostService, BlogPostService>();
 builder.Services.AddSingleton<IFriendLinkService, FriendLinkService>();
+builder.Services.AddSingleton(new LibraryConfiguration() { CollocatedJavaScriptQueryString = null });
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
 builder.Services.AddFluentUIComponents();
+builder.Services.AddSingleton(HtmlEncoder.Create(UnicodeRanges.All));
 
 var app = builder.Build();
 
