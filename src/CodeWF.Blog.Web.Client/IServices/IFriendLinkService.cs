0880c0e239e9e4fd6f960a006745533644394984
Dotnet9(dotnet9.com) <1012434131@qq.com>
2024-10-08 22:01:10 +0800

    clear;

--- src/CodeWF.Blog.Web.Client/IServices/IFriendLinkService.cs
+++
@@ -1,8 +0,0 @@
-﻿using CodeWF.Blog.Web.Client.Models.FriendLinks;
-
-namespace CodeWF.Blog.Web.Client.IServices;
-
-public interface IFriendLinkService
-{
-    Task<List<FriendLink>?> GetFriendLinksAsync();
-}
--- src/CodeWF.Blog.Web.Client/Options/SiteOption.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CodeWF.Blog.Web.Client.Options;
-
-public class SiteOption
-{
-    public string? Owner { get; set; }
-    public string? LocalAssetsDir { get; set; }
-}
