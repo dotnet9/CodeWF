31a2dd367eb6cbee7bf82bb0550ffec0d8789afd
Dotnet9(dotnet9.com) <1012434131@qq.com>
2024-10-27 12:25:49 +0800

    add post footer;

--- src/CodeWF/Extensions/ConstantUtil.cs
+++ src/CodeWF/Extensions/ConstantUtil.cs
@@ -4,7 +4,7 @@ public static class ConstantUtil
 {
     public const string DefaultCategory = "default";
     public static string GetBbsCategoryUrl(string slug) => $"/bbs/cat/{slug}";
-    public static string GetBbsPostUrl(BlogPost post) => $"./bbs/post/{post.Date?.Year}/{post.Date?.Month}/{post.Slug}";
+    public static string GetBbsPostUrl(BlogPost post) => $"/bbs/post/{post.Date?.Year}/{post.Date?.Month}/{post.Slug}";
 
     public static string GetPostGithubPath(string? githubRepository, BlogPost? post) =>
         $"{githubRepository}/blob/main/{post?.Date?.Year:D4}/{post?.Date?.Month:D2}/{post?.Slug}.md";
