d47d5b9bbac06e0095c0196d9ac3670737b89465
VleaStwo <78075553+VleaStwo@users.noreply.github.com>
2024-11-15 16:54:58 +0800

    🚀 settings 全局设置按钮组
    🐛 修复了toc对齐问题

---
+++ src/WebSite/wwwroot/js/settings.js
@@ -0,0 +1,45 @@
+﻿class Settings
+{
+    constructor()
+    {
+        this.defaultTimeout = 1500;
+    }
+
+    taskCompleted(btn, timeout)
+    {
+        if (btn)
+        {
+            btn.classList.add("checked");
+            setTimeout(() => btn.classList.remove("checked"), timeout ?? this.defaultTimeout);
+        }
+    }
+
+    openBtns(containerid)
+    {
+        const container = document.getElementById(containerid);
+        container.classList.toggle("open");
+    }
+
+    goBack()
+    {
+        window.history.back();
+    }
+
+    goToTop()
+    {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth"
+        });
+    }
+
+    goToBottom()
+    {
+        window.scrollTo({
+            top: document.body.scrollHeight,
+            left: 0,
+            behavior: "smooth"
+        });
+    }
+}
