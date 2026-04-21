474f33966d3c5b995ff2a15af3d678b956eca048
Dotnet9(dotnet9.com) <1012434131@qq.com>
2026-04-21 16:25:07 +0800

    update post list cover;

--- src/WebApp/Pages/Bbs/Album/Index.cshtml.cs
+++ src/WebApp/Pages/Bbs/Album/Index.cshtml.cs
@@ -1,20 +1,25 @@
 using WebApp.Models;
 using WebApp.Services;
+using WebApp.Options;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
 
 namespace WebApp.Pages.Bbs.Album;
 
 public class IndexModel : PageModel
 {
     private readonly AppService _appService;
+    private readonly IOptions<SiteOption> _siteOption;
 
     public string AlbumName { get; set; } = "所有专辑";
     public List<BlogPost>? Posts { get; set; }
     public List<AlbumItem>? Albums { get; set; }
+    public string Owner => _siteOption.Value.Owner ?? "沙漠尽头的狼";
 
-    public IndexModel(AppService appService)
+    public IndexModel(AppService appService, IOptions<SiteOption> siteOption)
     {
         _appService = appService;
+        _siteOption = siteOption;
     }
 
     public async Task OnGetAsync(string slug)
