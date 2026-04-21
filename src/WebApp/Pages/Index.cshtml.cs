ed8b327190a34d0781c98edfc3961e7d86e6b7d8
Dotnet9(dotnet9.com) <1012434131@qq.com>
2026-04-21 17:50:02 +0800

    update style;

--- src/WebApp/Pages/Index.cshtml.cs
+++ src/WebApp/Pages/Index.cshtml.cs
@@ -10,6 +10,8 @@ public class IndexModel : PageModel
     private readonly AppService _appService;
 
     public List<BlogPost>? Posts { get; set; }
+    public List<AlbumItem>? Albums { get; set; }
+    public List<CategoryItem>? Categories { get; set; }
 
     public IndexModel(AppService appService)
     {
@@ -19,5 +21,7 @@ public class IndexModel : PageModel
     public async Task OnGetAsync()
     {
         Posts = await _appService.GetBannerPostAsync();
+        Albums = await _appService.GetAllAlbumItemsAsync();
+        Categories = await _appService.GetAllCategoryItemsAsync();
     }
 }
