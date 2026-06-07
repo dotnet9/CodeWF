959940543f1fdb4b55d9eed429bc9c5da726399e
Dotnet9(dotnet9.com) <1012434131@qq.com>
2026-06-07 09:11:43 +0800

    Update vite.config.ts

--- src/CodeWF.Admin/vite.config.ts
+++ src/CodeWF.Admin/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/admin/",
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1100,
